91d42a98f27c8355787b0604e585ce4252db7599
John James Jacoby <johnjamesjacoby@me.com>
2016-01-07 23:18:46 -0600

    Update plugin header & docs.

--- wp-term-visibility.php
+++ wp-term-visibility.php
@@ -3,11 +3,14 @@
 /**
  * Plugin Name: WP Term Visibility
  * Plugin URI:  https://wordpress.org/plugins/wp-term-visibility/
- * Description: Visibility for categories, tags, and other taxonomy terms
  * Author:      John James Jacoby
- * Version:     0.2.0
  * Author URI:  https://profiles.wordpress.org/johnjamesjacoby/
- * License:     GPL v2 or later
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Description: Visibility for categories, tags, and other taxonomy terms
+ * Version:     0.2.0
+ * Text Domain: wp-term-visibility
+ * Domain Path: /assets/lang/
  */
 
 // Exit if accessed directly
@@ -23,10 +26,12 @@ function _wp_term_visibility() {
 	// Setup the main file
 	$plugin_path = plugin_dir_path( __FILE__ );
 
-	// Include the main class
-	require_once $plugin_path . '/includes/functions.php';
+	// Classes
 	require_once $plugin_path . '/includes/class-wp-term-meta-ui.php';
 	require_once $plugin_path . '/includes/class-wp-term-visibility.php';
+
+	// Functions
+	require_once $plugin_path . '/includes/functions.php';
 }
 add_action( 'plugins_loaded', '_wp_term_visibility' );
 
