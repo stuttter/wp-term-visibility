91cb94c2381c3596a713dc5f58395dca1d629e1c
John James Jacoby <johnjamesjacoby@me.com>
2016-01-07 18:22:45 -0600

    Update readme's and loading process.

--- wp-term-visibility.php
+++ wp-term-visibility.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://wordpress.org/plugins/wp-term-visibility/
  * Description: Visibility for categories, tags, and other taxonomy terms
  * Author:      John James Jacoby
- * Version:     0.1.3
+ * Version:     0.2.0
  * Author URI:  https://profiles.wordpress.org/johnjamesjacoby/
  * License:     GPL v2 or later
  */
@@ -14,27 +14,30 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Instantiate the main WordPress Term Color class
+ * Include the main WordPress Term Visibility files
  *
  * @since 0.1.2
  */
 function _wp_term_visibility() {
 
-	// Bail if no term meta
-	if ( ! function_exists( 'add_term_meta' ) ) {
-		return;
-	}
-
 	// Setup the main file
-	$file = __FILE__;
-	$dir  = dirname( $file );
+	$plugin_path = plugin_dir_path( __FILE__ );
 
 	// Include the main class
-	include $dir . '/includes/functions.php';
-	include $dir . '/includes/class-wp-term-meta-ui.php';
-	include $dir . '/includes/class-wp-term-visibility.php';
+	require_once $plugin_path . '/includes/functions.php';
+	require_once $plugin_path . '/includes/class-wp-term-meta-ui.php';
+	require_once $plugin_path . '/includes/class-wp-term-visibility.php';
+}
+add_action( 'plugins_loaded', '_wp_term_visibility' );
+
+/**
+ * Instantiate the main WordPress Term Images class
+ *
+ * @since 0.2.0
+ */
+function _wp_term_visibility_init() {
 
 	// Instantiate the main class
-	new WP_Term_Visibility( $file );
+	new WP_Term_Visibility( __FILE__ );
 }
-add_action( 'init', '_wp_term_visibility', 78 );
+add_action( 'init', '_wp_term_visibility_init', 75 );
